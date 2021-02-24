5de92258ca2b028e89b0ba95cc09dfc06b859b9c
michaljarnot <m.jarnot@yahoo.com>
2021-02-24 20:43:29 +0100

    fix: config
    - jest
    - husky

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@collection-nest/(.*)$': '<rootDir>/packages/$1/src'
+  },
+  modulePathIgnorePatterns: ['lib', 'dist'],
 }
