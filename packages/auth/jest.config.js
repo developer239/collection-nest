af77d42371a4b744345fac7ad96b8e72af13fbaf
michaljarnot <m.jarnot@yahoo.com>
2021-02-26 15:17:20 +0100

    refactor: make IDEA test runner work

--- packages/auth/jest.config.js
+++ packages/auth/jest.config.js
@@ -1,3 +1,11 @@
 const sharedConfig = require('../../jest.config.js');
+const { pathsToModuleNameMapper } = require('ts-jest/utils')
+const { compilerOptions } = require('./tsconfig')
 
-module.exports = sharedConfig
+module.exports = {
+  ...sharedConfig,
+  roots: ['../../'],
+  modulePaths: ['../../'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+  modulePathIgnorePatterns: ['lib', 'dist'],
+}
