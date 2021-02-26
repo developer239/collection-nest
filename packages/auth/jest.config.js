3934eb0c0865a6853c19ec27cc4e927a5c9e9e9f
michaljarnot <m.jarnot@yahoo.com>
2021-02-26 15:22:02 +0100

    refactor: absolute imports all in root

--- packages/auth/jest.config.js
+++ packages/auth/jest.config.js
@@ -1,11 +1,7 @@
 const sharedConfig = require('../../jest.config.js');
-const { pathsToModuleNameMapper } = require('ts-jest/utils')
-const { compilerOptions } = require('./tsconfig')
 
 module.exports = {
   ...sharedConfig,
   roots: ['../../'],
   modulePaths: ['../../'],
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
-  modulePathIgnorePatterns: ['lib', 'dist'],
 }
