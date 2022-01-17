038dbecab7ce630eec14f6e44c786746a02c3c5e
michaljarnot <m.jarnot@yahoo.com>
2022-01-17 01:25:06 +0100

    fix: make environment module configurable

--- packages/auth/src/welcome.module.ts
+++ packages/auth/src/welcome.module.ts
@@ -1,9 +1,23 @@
+/* eslint-disable security/detect-object-injection */
 import { Module } from '@nestjs/common'
 import { EnvironmentModule } from '@collection-nest/environment'
 import { WelcomeController } from 'auth/welcome.controller'
 
+const envFilePath = {
+  production: '.env.production',
+  development: '.env.development',
+  test: '.env.test',
+  ci: '.env.ci',
+}
+
+const NODE_ENV = process.env.NODE_ENV as keyof typeof envFilePath
+
 @Module({
-  imports: [EnvironmentModule],
+  imports: [
+    EnvironmentModule.register({
+      envFilePath: envFilePath[NODE_ENV],
+    }),
+  ],
   controllers: [WelcomeController],
   providers: [],
 })
--- packages/environment/src/lib/environment.module.ts
+++ packages/environment/src/lib/environment.module.ts
@@ -1,30 +1,25 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import * as Joi from 'joi'
 import { EnvironmentService } from 'environment/lib/environment.service'
 
-const NODE_ENV = process.env.NODE_ENV as keyof typeof envFilePath
-
-// TODO: make configurable
-const envFilePath = {
-  production: '.env.production',
-  development: '.env.development',
-  test: '../../../.env.test', // this is because of NX
-  ci: '.env.ci',
+export interface IConfig {
+  envFilePath: string
+  validationSchema?: any
 }
 
-@Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: envFilePath[NODE_ENV],
-      // TODO: make configurable
-      validationSchema: Joi.object({
-        DATABASE_URL: Joi.string().required(),
-        PORT: Joi.number(),
-      }),
-    }),
-  ],
-  providers: [EnvironmentService],
-  exports: [EnvironmentService],
-})
-export class EnvironmentModule {}
+@Module({})
+export class EnvironmentModule {
+  static register({ envFilePath, validationSchema }: IConfig) {
+    return {
+      module: EnvironmentModule,
+      imports: [
+        ConfigModule.forRoot({
+          envFilePath,
+          validationSchema,
+        }),
+      ],
+      providers: [EnvironmentService],
+      exports: [EnvironmentService],
+    }
+  }
+}
